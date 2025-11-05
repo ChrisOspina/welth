f4f459a026fc4805efea5c86ac2f868fad68e81d
ChrisOspina <ChrisOspina99@gmail.com>
2025-11-05 16:01:52 -0500

    scaffolding

---
+++ components/header.jsx
@@ -0,0 +1,17 @@
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
