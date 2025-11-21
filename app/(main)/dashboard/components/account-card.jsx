03fb56cb8f280a43d71ef2848c4a0ea6d3cf872f
ChrisOspina <ChrisOspina99@gmail.com>
2025-11-21 17:07:00 -0500

    Working on account js server is still broken

---
+++ app/(main)/dashboard/components/account-card.jsx
@@ -0,0 +1,54 @@
+"use client";
+import { ArrowUpRight, ArrowDownRight, CreditCard } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { Badge } from "@/components/ui/badge";
+import { useEffect } from "react";
+import useFetch from "@/hooks/use-fetch";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { updateDefaultAccount } from "@/actions/account";
+import { toast } from "sonner";
+import React from "react";
+
+const AccountCard = ({ account }) => {
+  const { name, type, balance, id, isDefault } = account;
+
+  const {
+    loading: updateDefaultLoading,
+    fn: updateDefaultFn,
+    data: updatedAccount,
+    error,
+  } = useFetch(updateDefaultAccount);
+
+  const handleDefaultChange = async (event) => {
+    event.preventDefault();
+
+    if (isDefault) {
+      toast.warning("You need at least 1 default account");
+      return;
+    }
+    await updateDefaultFn(id);
+  };
+
+  useEffect(() => {
+    if (updatedAccount?.success) {
+      toast.success("Default account updated successfully");
+    }
+  }, [updatedAccount]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message || "Failed to update default account");
+    }
+  }, [error]);
+
+  return <div>AccountCard</div>;
+};
+
+export default AccountCard;
